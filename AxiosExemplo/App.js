7e118811032c8d5133c5bb9117644464293ce85f
Jean Cabral <jean@popcode.com.br>
2017-10-09 16:14:42 -0300

    Ciclo de Vida do Componente

--- AxiosExemplo/App.js
+++ AxiosExemplo/App.js
@@ -3,11 +3,30 @@ import {View} from 'react-native';
 import ListaItens from './src/components/ListaItens';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    console.debug("Construindo a aplicação");
+  }
+
+  componentWillMount(){
+    console.debug("Fazendo alguma coisa antes de renderizar");
+  }
+
+  componentDidMount(){
+    console.debug("Fazendo alguma coisa DEPOIS de renderizar");
+  }
+  
+
+
   render() {
+    const exemplo = "Exemplo para aparecer no Console";
+    console.debug(exemplo);
     return (
       <View>
-      <ListaItens />
-      <ListaItens />
+        <ListaItens />
+        <ListaItens />
+        <ListaItens />
       </View>
     );
   }
