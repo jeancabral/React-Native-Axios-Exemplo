c05cd2f9074043821af161b43a44ec46096cf274
Jean Cabral <jeancabral@outlook.com>
2017-10-11 14:30:19 -0300

    Testando a Requisição HTTP com Axios

--- AxiosExemplo/src/components/ListaItens.js
+++ AxiosExemplo/src/components/ListaItens.js
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
 import { View } from "react-native";
+import axios from "axios";
+
 import Itens from "./Itens";
 
 export default class ListaItens extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { listaItens: [] };
+
+  }
+
+
+  componentWillMount() {
+    //Requisição HTTP
+    axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.php')
+      //.then(response => { console.log(response); })
+      .then(response => { this.setState({ listaItens: response.data }); })
+      .catch(() => { console.log('Erro ao recuperar os dados!'); })
+  }
+
+
   render() {
     return (
       <View>
-        <Itens />
-        <Itens />
-        <Itens />
+        {console.log(this.state.listaItens)}
       </View>
     );
   }
